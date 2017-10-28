ae04523b32482820ac0366fe98e1f5b547f0d066
Jihyun Kim <yhi00240@naver.com>
2017-10-28 20:50:30 +0900

    first commit

--- practice/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class TrainData(models.Model):
-    image = models.BinaryField()
-    label = models.IntegerField()
-
-# class TestData(models.Model):
-#     image = models.BinaryField()
-#     label = models.IntegerField()
